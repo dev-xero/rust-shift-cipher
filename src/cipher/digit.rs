8ba02f4b4a3ad86e96a1d52f3a487ae14c4e065e
Xero kun <elo48858@gmail.com>
2023-09-28 22:33:49 +0100

    feat: Defined decryption algorithms and internal functions

--- src/cipher/digit.rs
+++ src/cipher/digit.rs
@@ -10,3 +10,14 @@ pub fn encrypt(digit: char, shift: i8) -> Result<char, &'static str> {
 
     Ok(shifted_digit_char)
 }
+
+pub fn decrypt(digit: char, shift: i8) -> Result<char, &'static str> {
+    let digit = digit.to_digit(10)
+        .ok_or("Failed to parse digit")?;
+
+    let shifted_digit = (digit - (shift as u32)) % 10;
+    let shifted_digit_char = char::from_digit(shifted_digit as u32, 10)
+        .ok_or("Failed to parse digit")?;
+
+    Ok(shifted_digit_char)
+}
