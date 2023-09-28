1226eb45bf5e043e3e7eb165581db5326922cf63
Xero kun <elo48858@gmail.com>
2023-09-28 21:06:35 +0100

    feat: Implemented run with config

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{env, process::exit};
+use std::{env, process};
 
 use rust_shift_cipher::Config;
+use rust_shift_cipher::run;
 
 fn main() {
     let config = Config::build(env::args())
         .unwrap_or_else(|err| {
             eprintln!("Problem parsing arguments: {err}");
-            exit(1);
+            process::exit(1);
         });
     
-    println!("{}", config.text);
-    println!("{}", config.shift);
-    println!("{}", config.should_export);
+    if let Err(err) = run::with_config(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
+    }
 }
--- src/run/mod.rs
+++ src/run/mod.rs
@@ -3,13 +3,12 @@ use std::error::Error;
 use crate::Config;
 use crate::cipher;
 
-pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+pub fn with_config(config: Config) -> Result<(), Box<dyn Error>> {
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let result = cipher::encrypt(
-        &config.text, 
-        config.shift, 
-        alphabet
-    );
+    let result = cipher::encrypt(&config.text, config.shift, alphabet)?;
+
+    println!("Encrypting '{}'...", &config.text);
+    println!("Encrypted: '{}' (shift: {})", result, &config.shift);
 
     Ok(())
 }
