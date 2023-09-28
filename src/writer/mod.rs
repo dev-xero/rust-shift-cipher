2e6689de95cea9a60f722ca3ae9d3570e1914bb6
Xero kun <elo48858@gmail.com>
2023-09-28 21:34:25 +0100

    fix(bug): Create encryption results directory if absent

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,8 +1,18 @@
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 
 pub fn write_result(result: String, shift: &i8) -> Result<(), String> {
-    let file_path = format!("encrypted/decryption_key_{result}-{shift}.txt");
+    let file_path = format!("./encrypted/decryption_key_{result}-{shift}.txt");
+    let parent_dir = Path::new(&file_path).parent().unwrap();
+    
+    if !parent_dir.exists() {
+        match std::fs::create_dir_all(parent_dir) {
+            Ok(_) => println!("Created directories successfully."),
+            Err(e) => return Err(format!("Error creating directory: {}", e))
+        }
+    }
+
     let mut file = match File::create(&file_path) {
         Ok(file) => file,
         Err(e) => return Err(format!("Failed to write file to path: {}, error: {}", file_path, e))
