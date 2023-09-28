15c7e767c3007a7d67336ce721bf76590d830ba8
Xero kun <elo48858@gmail.com>
2023-09-28 20:48:32 +0100

    test: Added tests for encryption algorithm

---
+++ tests/cipher_tests.rs
@@ -0,0 +1,39 @@
+use rust_shift_cipher::cipher;
+
+#[test]
+fn test_encrypt() {
+    let encrypted_text = "xiaaewzl";
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let test = cipher::encrypt(String::from("password"), 8, alphabet)
+        .unwrap();
+
+    assert_eq!(encrypted_text, test);
+}
+
+#[test]
+fn test_encrypt_with_digit() {
+    let encrypted_text = "xiaaewzl6";
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let test = cipher::encrypt(String::from("password8"), 8, alphabet)
+        .unwrap();
+
+    assert_eq!(encrypted_text, test);
+}
+
+#[test]
+fn test_encrypt_digits_only() {
+    let encrypted_text = "111111111";
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let test = cipher::encrypt(String::from("999999999"), 2, alphabet)
+        .unwrap();
+
+    assert_eq!(encrypted_text, test);
+}
+
+#[test]
+#[should_panic]
+fn test_encrypt_with_invalid_string() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    cipher::encrypt(String::from("こんにちは"), 8, alphabet)
+        .unwrap();
+}
